9dd605fe8cb91771ee8b27fc04b721f1e0ee4cda
SilentFURY-x <tyagiarjun8109@gmail.com>
2026-02-19 13:29:04 +0530

    feat: Add command-execute button in the status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.command = 'my-repositories.openRepoPage';
+	statusBarItem.text = `$(github) My Repos`;
+	statusBarItem.tooltip = 'Click to open GitHub Repositories';
+	statusBarItem.show();
+
+context.subscriptions.push(statusBarItem);
 	
 	const disposable = vscode.commands.registerCommand('my-repositories.openRepoPage', () => {
 
